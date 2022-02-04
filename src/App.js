e8f31dca9353c7e4906c58a17319d2088806b1bb
Tim Copland <tim.copland@august.com.au>
2022-02-05 10:54:24 +1100

    Build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import logo from './logo.svg';
+import BabyYoda from './baby-yoda';
 import './App.css';
+import axios from 'axios';
+import { useState} from 'react';
+
+const url = 'https://sheet.best/api/sheets/b0f0eada-bf2e-40bf-ae70-eaf10af373ec';
 
 function App() {
+  const [state, setState] = useState(
+    {
+      id: new Date().getTime(),
+      name: '',
+      submitter: '',
+      isLoading: false,
+      hasError: false,
+      hasConfirm: false
+    }
+  );
+
+  const handleSubmit = function(e) {
+    setState({
+      ...state,
+      isLoading: true,
+      hasError: false,
+      hasConfirm: false
+    });
+      e.preventDefault();
+
+      if(state.name) {
+        axios.post(url, state)
+        .then(response => {
+          console.log(response);
+          setState({
+            ...state,
+            isLoading: false,
+            hasError: response.status !== 200,
+            hasConfirm: response.status === 200,
+          });
+        })
+      }
+  }
+
+  var rainbowClassnames = `rainbow-container ${state.isLoading ? 'is-loading' : ''}`;
+
+  const onInputChange = (e) => {
+    setState({
+      ...state,
+      id: new Date().getTime(),
+      [e.currentTarget.name]: e.currentTarget.value
+    });
+    console.log(state);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="baby-container">
+        <BabyYoda />
+      </div>
+      <h2>Name me you must</h2>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="submitter">I am </label>
+        <select name="submitter" id="submitter" onChange={onInputChange}>
+          <option value="">---</option>
+          <option value="Shaz">Shaz</option>
+          <option value="Kim">Kim</option>
+        </select>
+    
+        <label htmlFor="name">A good name would be: </label>
+        <input type="text" name="name" id="name" value={state.name} onChange={onInputChange}></input>
+
+
+        <button type="submit" disabled={!state.name || state.isLoading }>Save this name</button>
+      </form>
+
+      <div className="notification">
+        {state.hasError ? <p>Uh-oh, something went wrong...</p> : ''}
+        {state.hasConfirm ? <p>Success!</p> : ''}
+      </div> 
+
+      <div className={rainbowClassnames}>
+        <div className="rainbow"></div>
+      </div>
     </div>
   );
 }
---
+++ src/baby-yoda.js
@@ -0,0 +1,19 @@
+const BabyYoda = function(){
+    return (
+        <div id="y-body">
+            <div id="y-head">
+                <div id="left-eye"></div>
+                <div id="right-eye"></div>
+                <div id="mouth"></div>
+                <div id="left-ear"></div>
+                <div id="right-ear"></div>
+            </div>
+            <div id="cup">
+                <div id="steam">~</div>
+            </div>
+        </div>
+    )
+}
+
+export default BabyYoda;
+
